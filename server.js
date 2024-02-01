3244d0f80be8ada37259f3d3c19c650c06c53110
Jacob Humston <jhumston24@sciototech.org>
2024-02-01 14:14:23 -0500

    Fixed up the logo a little bit.

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 import express from 'express';
 const app = express();
 app.use('/', express.static('src/', { extensions: ['html'] }));
-app.get('/', function (_, response) {
-    response.redirect('/our-mission');
-});
 app.listen(80, () => console.log('Server is listening on port 80.'));
