9328a3c6193cea29fbc0a0219a56e36eee4a21b0
Jacob Humston <jhumston24@sciototech.org>
2024-02-01 13:44:43 -0500

    Got a lot of stuff working.

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ import express from 'express';
 const app = express();
 app.use('/', express.static('src/', { extensions: ['html'] }));
 app.get('/', function (_, response) {
-    response.redirect('/home');
+    response.redirect('/our-mission');
 });
 app.listen(80, () => console.log('Server is listening on port 80.'));
