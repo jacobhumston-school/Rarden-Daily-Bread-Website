10c2771a42a5d844be4f7176cbf1f0f407288b26
Jacob Humston <jhumston24@sciototech.org>
2024-02-08 12:50:36 -0500

    Fixed up the footer a little bit.
    Co-authored-by: anteatersarecool <anteatersarecool@users.noreply.github.com>
    Co-authored-by: possbum <possbum@users.noreply.github.com>

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,21 @@
  * All rights reserved.
  */
 
+console.time("Load Time")
+
 function main() {
     document.title = `${document.title} | Rarden Daily Bread`;
+
+    {
+        const footerCopyRight = document.getElementById('footer-copyright');
+        if (footerCopyRight !== null) {
+            const date = new Date();
+            const year = date.getFullYear();
+            footerCopyRight.innerText = footerCopyRight.innerText.replace('2024', year);
+        }
+    }
+
+    console.timeEnd("Load Time")
 }
 
 window.addEventListener('load', main);
